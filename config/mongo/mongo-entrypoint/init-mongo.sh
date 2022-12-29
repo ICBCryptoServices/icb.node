ff0b3aa1953ba583e1e82f240b706481955293d1
Sorosh saloty <sorosh.saloti@pishtazan.onmicrosoft.com>
2022-12-29 18:02:29 +0300

    change mongodb script

--- config/mongo/mongo-entrypoint/init-mongo.sh
+++ config/mongo/mongo-entrypoint/init-mongo.sh
@@ -3,15 +3,14 @@ mongo -- "$MONGO_INITDB_ROOT_DATABASE" <<EOF
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
     var admin = db.getSiblingDB('$MONGO_INITDB_ROOT_DATABASE');
     admin.auth(rootUser, rootPassword);
-
     var user = '$MONGO_INITDB_USERNAME';
     var passwd = '$MONGO_INITDB_PASSWORD';
-    db.createUser({user: user, pwd: passwd, roles: [{role: "readWrite", db: "$MONGO_INITDB_DATABASE"},
-    {role: "dbAdmin", db: "$MONGO_INITDB_DATABASE"},
-    {role: "dbOwner", db: "$MONGO_INITDB_DATABASE"}]});
-
-    var chaindb=db.getSiblingDB('$MONGO_INITDB_DATABASE');
-    chaindb.auth(rootUser, rootPassword);
+    var chaindb='$MONGO_INITDB_DATABASE';
+    db.createUser({user: user, pwd: passwd, roles: [{role: 'readWrite', db: chaindb },
+      {role: 'dbAdmin', db: chaindb},
+      {role: 'dbOwner', db: chaindb}]});
+    var chaindb=db.getSiblingDB(chaindb);
+    chaindb.auth(user, passwd);
     chaindb.createCollection('tbl_blocks');
     chaindb.createCollection('tbl_txns');
     chaindb.createCollection('tbl_txns_pool');
@@ -19,5 +18,4 @@ mongo -- "$MONGO_INITDB_ROOT_DATABASE" <<EOF
     chaindb.createCollection('tbl_peers');
     chaindb.createCollection('tbl_accounts');
     chaindb.createCollection('tbl_sysLogs');
-
 EOF
